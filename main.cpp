daf7e2a9408e812dfc6d883ec99d65c15dbd04ad
vsvcea <vsvcea@bk.ru>
2019-10-21 00:55:06 +1000

    fullproject

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
- #include <Windows.h>
+#include <Windows.h>
 #include <conio.h>
 #include <iostream>
 
@@ -12,4 +12,18 @@ int main()
 		b++;
 		a = _getch();
 		if (!(b % 2))
-		{
+	        { 
+			if (a >= int('A') && a < int('Z'))
+			{
+				a = a + int('a') - int('A');
+			}
+			else (a >= int('a') && a <= int('z'));
+			{
+			a = a + int('A') - int('a');
+			}
+		}
+
+        	cout << char(a);
+	}
+	return 0;
+}
